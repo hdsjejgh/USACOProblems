8c079cb06032b024d1631cdb437bc5fb60b5fcef
hdsjejgh <140638415+hdsjejgh@users.noreply.github.com>
2025-03-19 17:57:05 -0400

    Add files via upload

--- 2023BronzeFeb3.py
+++ 2023BronzeFeb3.py
@@ -1,17 +1,6 @@
-import sys
-
-sys.stdin = open("cowqueue.in", "r")
-sys.stdout = open("cowqueue.out", "w")
-
-N = int(input())
-times = []
-for i in range(N):
-    times.append(list(map(int,input().split())))
-times.sort()
-currentTime=0
-for i in times:
-    if i[0]>currentTime:
-        currentTime=i[0]+i[1]
-    else:
-        currentTime+=i[1]
-print(currentTime)
+N, K = list(map(int,input().split()))
+N+=K
+schedule = list(map(int,input().split()))
+for i in range(len(schedule)-1):
+    N+=min(schedule[i+1]-schedule[i]-1,K)
+print(N)
